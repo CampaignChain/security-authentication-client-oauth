4ce6d1a91990717f20eea63107c7128580bf9b45
Sandro Groganz <sandro@campaignchain.com>
2014-11-20 17:51:25 +0100

    CE-9 Created + Last Updated attributes for entities

--- Entity/Application.php
+++ Entity/Application.php
@@ -10,13 +10,14 @@
 
 namespace CampaignChain\Security\Authentication\Client\OAuthBundle\Entity;
 
+use CampaignChain\CoreBundle\Entity\Meta;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity
  * @ORM\Table(name="campaignchain_security_authentication_client_oauth_application")
  */
-class Application
+class Application extends Meta
 {
     /**
      * @ORM\OneToMany(targetEntity="Token", mappedBy="application")
