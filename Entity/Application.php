db7a1bfae4f3cabf8c87b497180fe6a29b0cb585
Sandro Groganz <sandro@campaignchain.com>
2015-01-14 17:13:26 +0100

    CE-226 Allow secret field to be empty for OAuth application (in form and DB)

--- Entity/Application.php
+++ Entity/Application.php
@@ -42,7 +42,7 @@ class Application extends Meta
     protected $key;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, nullable=true)
      */
     protected $secret;
 
